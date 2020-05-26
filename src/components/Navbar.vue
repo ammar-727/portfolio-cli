dd8d1268aacd583c37d7947b606c0416881b2d88
ammar-727 <ammarjawed.1111@gmail.com>
2020-05-26 13:17:40 +0500

    LinkedIn Link Added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
                     <b-navbar-nav>
                         <b-nav-item href="https://www.instagram.com/ammar_727/" target='_blank'><img src="../assets/insta.png" width="30"></b-nav-item>
                         <b-nav-item href="https://web.facebook.com/ammar.jawed.522" target='_blank'><img src="../assets/fb.png" width="30"></b-nav-item>
-                        <b-nav-item href="https://www.pornhub.com/" target='_blank'><img src="../assets/in.png" width="30"></b-nav-item>
+                        <b-nav-item href="https://pk.linkedin.com/" target='_blank'><img src="../assets/in.png" width="30"></b-nav-item>
                         <b-nav-item href="#contact">Contact</b-nav-item>
                     </b-navbar-nav>
                 </b-navbar-nav>
